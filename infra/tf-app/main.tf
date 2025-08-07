61395562880b10e6dd6505d7386285eade57b039
@khad0062 <khad0062@algonquinlive.com>
2025-08-07 00:09:02 -0400

    Add remaining terraform files and documentation

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,16 +1,9 @@
 
-# Main Terraform configuration for the app infrastructure
-# This file contains the primary resources for the application
-
-# Resource group for the application infrastructure
 resource "azurerm_resource_group" "app" {
   name     = "khad0062-a12-rg"
   location = "East US"
 
-  tags = {
-    purpose     = "application-infrastructure"
-    environment = "prod"
-    created_by  = "terraform"
-    lab         = "a12"
-  }
 }
+
+
+
