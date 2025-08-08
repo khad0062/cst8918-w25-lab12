5c2ad12621972cf4de2f375395e55fa6d8f69c17
@khad0062 <khad0062@algonquinlive.com>
2025-08-08 19:07:03 -0400

    Add variables.tf file to satisfy terraform_standard_module_structure lint rule

---
+++ infra/tf-app/variables.tf
@@ -0,0 +1,5 @@
+# Variables for the app infrastructure
+# This file contains input variables for the Terraform configuration
+
+# Currently no variables are defined, but this file is required
+# by terraform_standard_module_structure linting rules
